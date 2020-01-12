e881deb7291d05bdcb489c8a85d54e878ef31286
Naeemhaq <naeemhaq@gmail.com>
2020-01-11 23:06:47 -0500

    version 1 of the component

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        imageUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
+
+    render() { 
+    
+        return(
+            <React.Fragment>
+                
+                <span style= {this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
+            </React.Fragment>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+ 
+export default Counter;
