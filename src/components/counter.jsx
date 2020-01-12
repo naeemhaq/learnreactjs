0b451b2f6b2ed33221310d6dd942167de5287eb6
Naeemhaq <naeemhaq@gmail.com>
2020-01-11 23:12:13 -0500

    conditional renderring content version 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,34 +7,18 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
-    }
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>there are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
 
+    }
     render() { 
     
         return(
             <React.Fragment>
-                
-                <span style= {this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                {this.renderTags()};
             </React.Fragment>
         );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
     }
 }
  
