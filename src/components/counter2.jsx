715abdeddaf3471ecb126d408834ee4565ef886d
Naeemhaq <naeemhaq@gmail.com>
2020-01-12 20:39:35 -0500

    what happens when state changes 1:11

--- src/components/counter2.jsx
+++ src/components/counter2.jsx
@@ -6,7 +6,7 @@ class Counter2 extends Component {
         imageUrl: 'https://picsum.photos/200',
         tags: []
     };
-
+/*
     constructor () {
         super(); // call the constructor of parent class
         // with the bind method you can set the value of this. 
@@ -14,8 +14,12 @@ class Counter2 extends Component {
         // in which this will always reference to the current object
         this.handleIncrement = this.handleIncrement.bind(this);
     }
-    handleIncrement() {
-        console.log('Increment Clicked', this);
+*/
+    // the above constructor is not required if we convert below to an arrow function
+    handleIncrement = () => {
+        // setState is inherited method which updates the virtual DOM and 
+        // view is updated through this method. 
+        this.setState({count: this.state.count + 1});
     }
 
     render() { 
